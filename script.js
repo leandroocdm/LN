df221f57f15984067c0fe522c94e27d11f2f16b4
Leandro Cardoso de Moraes <57853689+leandroocdm@users.noreply.github.com>
2026-03-27 10:57:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function toggleMenu() {
 const carousel = document.querySelector(".grid-servicos");
 
     let scrollAmount = 0;
-    const cardWidth = (carousel.clientWidth+15); 
+    const cardWidth = (carousel.clientWidth+20); 
     const delay = 3000;
 
     function autoScroll(){
